984ed5e006849c055328397b25423489e73bc899
vrrodgers <trulymissvicky1982@gmail.com>
2017-05-01 14:22:41 -0500

    implemented crud function for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @products = Product.all
   end
 
 
@@ -7,25 +10,49 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product= Product.new
   end
 
 
   def create
+    @product = Product.new(product_params)
   end
 
 
   def edit
+
   end
 
   def update
+    if @product.update(product_params)
+      flash[:success] = "Product was updated successfully!"
+      redirect_to product_path(@product)
+    else
+      render 'edit'
+    end  
   end
   
   def destroy
+    @product.destroy
   end
 
 
+  private
+    def set_product
+      @products = Product.find(params[:id])
+    end
 
+    def product_params
+      params.require(:recipe).permit(:name,
+                                    :image,
+                                    :description,
+                                    :price,
+                                    :catagory_id,
+                                    :inventory)
 
+    end
+
+      
 
 
 end
