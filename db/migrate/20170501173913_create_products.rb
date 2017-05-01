9ec8d67788e2f369538cbeacb2fc0d6fe35c0649
vrrodgers <trulymissvicky1982@gmail.com>
2017-05-01 13:31:24 -0500

    add crud to products

--- db/migrate/20170501173913_create_products.rb
+++ db/migrate/20170501173913_create_products.rb
@@ -1,7 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-
+       t.string :image
+       t.string :name
+       t.text :description
+       t.integer :price
+       t.integer :catagory_id
+       t.integer :inventory
       t.timestamps
     end
   end
